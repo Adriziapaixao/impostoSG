d9138c55be116e59c5771261d4a4c7c717419f24
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-12 15:31:36 -0300

    feat(entities): adicionar RoleEntity com anotações JPA"

---
+++ src/main/java/com/zup/impostoSG/entities/RoleEntity.java
@@ -0,0 +1,23 @@
+package com.zup.impostoSG.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public RoleEntity(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+}
