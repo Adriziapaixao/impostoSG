c141026dedfc0c2076495f7cc7bf99c7ef56c6a0
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-12 13:51:36 -0300

    chore: inicializa projeto spring initializr com dependências spring security, data, validation, web, postgres e lombok

---
+++ src/test/java/com/zup/impostoSG/ImpostoSgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.impostoSG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostoSgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
