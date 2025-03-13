76bbefe9652f0e6ab61eb315af96ef4e357c4bbe
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-13 10:34:36 -0300

    Feat: cria os repositórios UserRepository.java, TipoImpostoRepository.java e RolesRepository.java responsáveis pelos por acessar o banco de dados e realizar as operações de CRUD com as entidades UserEntity, TipoImpostoEntity e RoleEntity.

---
+++ src/main/java/com/zup/impostoSG/repositories/TipoImpostoRepository.java
@@ -0,0 +1,8 @@
+package com.zup.impostoSG.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoImpostoRepository extends JpaRepository {
+}
---
+++ src/main/java/com/zup/impostoSG/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.zup.impostoSG.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository {
+
+}
